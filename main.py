a3d31ebe60899ffd5dcca86dbee051359eb2d510
xprilion <xprilion@gmail.com>
2023-12-02 00:05:36 +0800

    setup

---
+++ main.py
@@ -0,0 +1,23 @@
+import google.generativeai as genai
+
+genai.configure(api_key="YOUR API KEY")
+
+defaults = {
+  'model': 'models/text-bison-001',
+  'temperature': 0.7,
+  'candidate_count': 1,
+  'top_k': 40,
+  'top_p': 0.95,
+  'max_output_tokens': 1024,
+  'stop_sequences': [],
+  'safety_settings': [{"category":"HARM_CATEGORY_DEROGATORY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_TOXICITY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_VIOLENCE","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_SEXUAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_MEDICAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_DANGEROUS","threshold":"BLOCK_MEDIUM_AND_ABOVE"}],
+}
+
+prompt = f""""""
+
+response = genai.generate_text(
+  **defaults,
+  prompt=prompt
+)
+
+print(response.result)
