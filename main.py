295c24f6f2df8c059d8b901798328254e52df99d
xprilion <xprilion@gmail.com>
2023-12-02 00:44:45 +0800

    Test connection with OAuth

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
 import google.generativeai as genai
-from keys import api_key
 
-genai.configure(api_key=api_key)
-
-defaults = {
-  'model': 'models/text-bison-001',
-  'temperature': 0.7,
-  'candidate_count': 1,
-  'top_k': 40,
-  'top_p': 0.95,
-  'max_output_tokens': 1024,
-  'stop_sequences': [],
-  'safety_settings': [{"category":"HARM_CATEGORY_DEROGATORY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_TOXICITY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_VIOLENCE","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_SEXUAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_MEDICAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_DANGEROUS","threshold":"BLOCK_MEDIUM_AND_ABOVE"}],
-}
-
-prompt = f"""Write a story about a magic backpack
-"""
-
-response = genai.generate_text(
-  **defaults,
-  prompt=prompt
-)
-
-print(response.result)
+for i, m in zip(range(5), genai.list_tuned_models()):
+    print(m.name)
