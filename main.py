16a2fa6045e27dd5e058a24b8b53436d5f868a00
xprilion <xprilion@gmail.com>
2023-12-02 00:10:40 +0800

    foundation-model-sample

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
+from keys import api_key
 
-genai.configure(api_key="YOUR API KEY")
+genai.configure(api_key=api_key)
 
 defaults = {
   'model': 'models/text-bison-001',
@@ -13,7 +14,8 @@ defaults = {
   'safety_settings': [{"category":"HARM_CATEGORY_DEROGATORY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_TOXICITY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_VIOLENCE","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_SEXUAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_MEDICAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_DANGEROUS","threshold":"BLOCK_MEDIUM_AND_ABOVE"}],
 }
 
-prompt = f""""""
+prompt = f"""Write a story about a magic backpack
+"""
 
 response = genai.generate_text(
   **defaults,
