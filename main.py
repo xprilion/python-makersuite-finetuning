a18281f6693f8f976ad961a4c2685d3d563c0758
xprilion <xprilion@gmail.com>
2023-12-02 00:50:00 +0800

    Get result from fine tuned model

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import google.generativeai as genai
 
-for i, m in zip(range(5), genai.list_tuned_models()):
-    print(m.name)
+chat_prompt = """product_name: MagicMobile
+product_description: Introducing MagicMobile, the future of transportation and communication combined! Seamlessly switch between a high-end mobile phone and a stylish five-wheeled car. With cutting-edge technology and a dash of magic, MagicMobile adapts to your needs, wherever you are.
+reviewer: Henry Paddock"""
+
+completion = genai.generate_text(
+                model=f'tunedModels/devfesthkprodreviews-p989bn7rdqwf',
+                prompt=chat_prompt
+            )
+
+print(completion.result)
