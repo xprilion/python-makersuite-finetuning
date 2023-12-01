965e49a52b0cfd764b512616346af20918f722bf
xprilion <xprilion@gmail.com>
2023-12-02 00:54:45 +0800

    better auth mechanism in place

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import google.generativeai as genai
 
+from load_creds import load_creds
+
+creds = load_creds()
+
+genai.configure(credentials=creds)
+
 chat_prompt = """product_name: MagicMobile
 product_description: Introducing MagicMobile, the future of transportation and communication combined! Seamlessly switch between a high-end mobile phone and a stylish five-wheeled car. With cutting-edge technology and a dash of magic, MagicMobile adapts to your needs, wherever you are.
 reviewer: Henry Paddock"""
